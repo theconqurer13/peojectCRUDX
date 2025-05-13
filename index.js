123c788897c173b6f2fb29a3274db93e4082e721
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-05-13 23:52:04 +0530

    first commit project skeleton setup

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-
+const {PORT} = require('./config/server-config');
 
 const prepareAndStartServer = () =>{
     app.listen(3000,()=>{
-        console.log("Server started at port ",3000);
+        console.log(`Server started at port ${PORT}`);
     })
 }
 
