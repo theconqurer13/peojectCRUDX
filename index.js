f29005087f6fc66b05173d893bcb1af70c8b4409
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-05-13 19:25:09 +0530

    setting up the project

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
+
+
+
+const prepareAndStartServer = () =>{
+    app.listen(3000,()=>{
+        console.log("Server started at port ",3000);
+    })
+}
+
+prepareAndStartServer();
